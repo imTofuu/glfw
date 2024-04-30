b0aff28efe04165735e91c2da42dd0ba0b99e8fd
Drew <117977057+imTofuu@users.noreply.github.com>
2024-04-30 22:32:24 +1200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,61 @@
+project: "GLFW"
+  kind: "StaticLib"
+  language: "C"
+  warnings: "off"
+
+  targetdir("bin/" .. outputdir .. "/%{prj.name}")
+  objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "include/GLFW/*.h",
+  }
+
+  filter "system:windows"
+    pic: "On"
+
+    files {
+      "src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+    }
+
+    defines {
+      "_GLFW_WIN32"
+    }
+
+  filter "system:linux
+    pic: "On"
+
+    files {
+      "src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_module.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+    }
+
+    defines {
+      "_GLFW_X11"
+    }
+
+  filter "configurations:Debug"
+    runtime: "Debug"
+    symbols "On"
+
+  filter "configurations:Release"
+    runtime: "Release"
+    optimise: "Speed"
